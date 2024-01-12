e16d7434817be129220ee7eb535a890ccdc9916e
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-01-12 19:59:02 -0300

    signup

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,11 +1,18 @@
 import { db } from "../database/db.js";
-import { userSchema } from "../schemas/user.schema.js";
+import bcrypt from "bcrypt";
 
 export async function signUp(req, res, next) {
     const {name, email, password} = req.body
 
     try {
-        res.send("sign-up")
+        const user = await db.collection("users").findOne({ email })
+        if (user) return res.status(409).send({message: "User already exists"})
+
+        const hash = bcrypt.hashSync(password, 10)
+
+        await db.collection("users").insertOne({ name, email, password: hash })
+        res.sendStatus(201)
+        
     } catch (error) {
         res.status(500).send(error.message)
     }
