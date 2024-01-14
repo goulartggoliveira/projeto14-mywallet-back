0fb844958abc3ae5657d7a1d6825d98fdd82c045
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-01-14 20:39:12 -0300

    wallet post inputwallet finish

--- src/controllers/wallet.controllers.js
+++ src/controllers/wallet.controllers.js
@@ -1,14 +1,15 @@
 import { db } from "../database/db.js";
 import bcrypt from "bcrypt";
 import {v4 as uuid} from "uuid"
+import dayjs from "dayjs";
 
 export async function inputWallet(req, res, next) {
     const {value, description, type} = req.body
     const { token, userId } = res.locals
     try {
         const wallet = {value, description, type, date: dayjs().valueOf(), userId }
-        await db.collection("wallet")
-
+        await db.collection("wallet").insertOne(wallet)
+        res.sendStatus(200)
     } catch (error) {
         res.status(500).send(error.message);
     }
@@ -18,7 +19,6 @@ export async function inputWallet(req, res, next) {
 export async function visualWallet(req, res, next) {
 
     try {
-        res.send("post")
         
     } catch (error) {
         res.status(500).send(error.message);
