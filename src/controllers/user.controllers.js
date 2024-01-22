b7eae9646f2bb1a0922569396a2e22d0dce340a4
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-01-22 11:21:19 -0300

    message config

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -7,7 +7,7 @@ export async function signUp(req, res, next) {
 
   try {
     const user = await db.collection("users").findOne({ email });
-    if (user) return res.status(409).send({ message: "User already exists" });
+    if (user) return res.status(409).send("User already exists");
 
     const hash = bcrypt.hashSync(password, 10);
 
@@ -22,11 +22,11 @@ export async function signIn(req, res, next) {
   const { email, password } = req.body;
   try {
     const user = await db.collection("users").findOne({ email });
-    if (!user) return res.status(404).send({ message: "email not found" });
+    if (!user) return res.status(404).send("email not found");
 
     const passwordExists = bcrypt.compareSync(password, user.password);
     if (!passwordExists)
-      return res.status(401).send({ message: "incorrect password" });
+      return res.status(401).send("incorrect password");
 
     const token = uuid();
     await db.collection("sessions").insertOne({ token, userId: user._id });
--- src/middlewares/userTokenValidate.js
+++ src/middlewares/userTokenValidate.js
@@ -4,11 +4,11 @@ export async function userTokenValidate(req, res, next) {
   const { authorization } = req.headers;
   const token = authorization?.replace("Bearer ", "");
 
-  if (!token) return res.status(404).send({ message: "token not found" });
+  if (!token) return res.status(404).send("token not found");
 
   try {
     const session = await db.collection("sessions").findOne({ token });
-    if (!session) return res.status(404).send({ message: "invalid token" });
+    if (!session) return res.status(404).send("invalid token");
 
     res.locals.token = token;
     res.locals.userId = session.userId;
